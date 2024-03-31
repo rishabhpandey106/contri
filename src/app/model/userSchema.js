680ed1dd0023103e32833025942a66514aa621f6
Rishabh Kumar Pandey <118062111+rishabhpandey106@users.noreply.github.com>
2024-03-31 23:06:11 +0530

    Update userSchema.js

--- src/app/model/userSchema.js
+++ src/app/model/userSchema.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, "Please provide an email"],
-        unique: true
+        required: [true, "Please provide an email"]
     },
     quantity: {
         type: Number,
